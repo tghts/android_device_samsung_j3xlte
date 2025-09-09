df0fa322746e2c0fff63fcdbc7e8456cd5b018d6
tghts <69969100+tghts@users.noreply.github.com>
2025-09-09 22:29:19 +0200

    Update init_j3xlte.cpp

--- init/init_j3xlte.cpp
+++ init/init_j3xlte.cpp
@@ -35,14 +35,17 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-static void import_kernel_hwrev(const std::string& key, const std::string& value, bool for_emulator)
-{
-    if (key.empty()) return;
+#include <stdlib.h>
+#include <string.h>
 
-    if (key == "hw_revision") {
-        property_set("ro.revision", value.c_str());
-    }
-}
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <android-base/properties.h>
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
 
 void property_override(char const prop[], char const value[])
 {
@@ -55,60 +58,26 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[],
+        char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void vendor_load_properties()
 {
-    std::string platform = property_get("ro.board.platform");
-    if (platform.empty() || platform != ANDROID_TARGET)
-        return;
+    std::string bootloader = GetProperty("ro.bootloader", "");
+    std::string device;
 
-    import_kernel_cmdline(false, import_kernel_hwrev);
-    std::string revision = property_get("ro.revision");
-    if (revision.empty())
-        property_set("ro.revision", "0");
-    
-    char* simslot_count_path = (char *)"/proc/simslot_count";
-    // Create a two byte array, so we can use it as a string
-    char simslot_count[2] = "\0";
-    
-    // Open the file as read-only
-    FILE* file = fopen(simslot_count_path, "r");
-    if (file != NULL) {
-        simslot_count[0] = fgetc(file);
-        property_set("ro.multisim.simslotcount", simslot_count);
-        property_set("ro.msms.phone_count", simslot_count);
-        property_set("ro.modem.w.count", simslot_count);
-        property_set("persist.msms.phone_count", simslot_count);
-        if (simslot_count[0] == '2')
-            property_set("persist.radio.multisim.config", "dsds");
-
-        fclose(file);
+    if (bootloader.find("J320F") != std::string::npos) {
+        /* SM-J320F TODO: replace the fingerprints with the right ones */
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-J320F");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/j3xlte/j3xlte:10/NMF26X/J320W8VLU2BQK1:user/release-keys");
+        property_override("ro.system.build.fingerprint", "ro.system.build.fingerprint");
+        property_override("ro.build.description", "j3xlte-user 10 NMF26X J320W8VLU2BQK1 release-keys");
     }
 
-    std::string bootloader = property_get("ro.bootloader");
-    if (strstr(bootloader.c_str(), "J320F") == NULL) {
-        if (strstr(bootloader.c_str(), "J320G")) {
-            /* SM-J320G */
-            property_override("ro.product.model", "SM-J320G");
-            property_override("ro.product.name", "j3xltedx");
-            property_override("ro.build.description", 
-                "j3xltedx-user 5.1.1 LMY47V J320GXXS0AQL1 release-keys");
-            property_override("ro.build.fingerprint", 
-                "samsung/j3xltedx/j3xlte:5.1.1/LMY47V/J320GXXS0AQL1:user/release-keys");
-            property_override("ro.bootimage.build.fingerprint", 
-                "samsung/j3xltedx/j3xlte:5.1.1/LMY47V/J320GXXS0AQL1:user/release-keys");
-        } else {
-            /* SM-J320M */
-            property_override("ro.product.model", "SM-J320M");
-            property_override("ro.product.name", "j3xlteub");
-            property_override("ro.build.description", 
-                "j3xlteub-user 5.1.1 LMY47V J320MVJU0ARH1 release-keys");
-            property_override("ro.build.fingerprint", 
-                "samsung/j3xlteub/j3xlte:5.1.1/LMY47V/J320MVJU0ARH1:user/release-keys");
-            property_override("ro.bootimage.build.fingerprint", 
-                "samsung/j3xlteub/j3xlte:5.1.1/LMY47V/J320MVJU0ARH1:user/release-keys");
-        }
-    }
-
-    std::string device = property_get("ro.product.device");
-    INFO("Found bootloader id %s setting build properties for %s device\n", bootloader.c_str(), device.c_str());
+    device = GetProperty("ro.product.device", "");
+    LOG(ERROR) << "Found bootloader id '" << bootloader.c_str() << "' setting build properties for '" << device.c_str() << "' device\n";
 }
